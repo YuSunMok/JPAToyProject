bf7dd024629e9dfed8d55f0056d5ce4516649f2b
sunmok <tjsahr9191@naver.com>
2023-12-04 23:21:04 +0900

    refactor : MemberApiController saveMember Entity -> DTO

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -15,10 +15,24 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
-    @PostMapping("/api/v1/members")
-    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+//    @PostMapping("/api/v1/members")
+//    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+//        Long id = memberService.join(member);
+//        return new CreateMemberResponse(id);
+//    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
     }
 
     @Data
