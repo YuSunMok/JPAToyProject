acf942b6415a0b85210ecd0b5b23538e76b82e51
sunmok <tjsahr9191@naver.com>
2023-12-06 12:07:16 +0900

    feat : OrderSimpleApiController return simpleOrderDto

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -24,7 +24,7 @@ public abstract class Item {
     private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<Category>();
+    private List<Category> categories = new ArrayList<>();
 
     //==비즈니스 로직==//
 
